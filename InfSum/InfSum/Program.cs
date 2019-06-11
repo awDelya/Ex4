bb8afd6f21dabfb6feb4c72896575e6b1feb5216
Овчинникова Аделина <deley@mail.ru>
2019-06-12 01:48:40 +0500

    Add fuction Summa + bugs fix

--- InfSum/InfSum/Program.cs
+++ InfSum/InfSum/Program.cs
@@ -9,9 +9,25 @@ namespace InfSum
 {
     class Program
     {
+        static double Summa(int i)
+        {
+            double sum = Math.Pow(-1, i + 1) / (Math.Pow(i, 3) + 3 * Math.Pow(i, 2) + 2 * i);
+            return sum;
+        }
         static void Main()
         {
-
+            int i = 0;
+            double sum = 0;
+            Color.Print(" Введите заданную точность e = ", ConsoleColor.Yellow);
+            double e = Number.Check(0.000000001, 1);
+            do
+            {
+                i++;
+                sum = sum + Summa(i);
+            }
+            while (Math.Abs(Summa(i)) >= e);
+            Color.Print("\n Сумма = " + sum + " с точностью е = " + e + " (последнее слaгаемое " + Summa(i) + ")", ConsoleColor.Green);
+            Console.ReadKey();
         }
     }
 }
